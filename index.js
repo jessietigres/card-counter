f79c6c2252f2164c6c7989c6f45e5e8b4fd60065
jessietigres <83791083+jessietigres@users.noreply.github.com>
2023-02-02 00:19:58 -0600

    Nibiru

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let nibiru = document.getElementbyId("nib")
 let count = 0
 
 function increment() {
@@ -13,3 +14,9 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+function nib() {
+    count += 1
+    countEl.textContent = count
+}
+
